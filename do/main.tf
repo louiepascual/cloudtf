1acec0bd6b966da15b6c1ad4509623b594bf6084
Louie Pascual <louieepascual@gmail.com>
2024-02-06 22:58:27 +0800

    resize vm, add vol
    Signed-off-by: Louie Pascual <louieepascual@gmail.com>

--- do/main.tf
+++ do/main.tf
@@ -15,14 +15,35 @@ data "digitalocean_ssh_key" "ssh_key" {
     name = var.do_ssh_key
 }
 
+# resource "digitalocean_droplet" "chromedp_build" {
+#     image  = "debian-12-x64"
+#     name   = "chromedp-build"
+#     region = "sgp1"
+#     size   = "c-16"
+#     ssh_keys = [data.digitalocean_ssh_key.ssh_key.id]
+# }
+
+resource "digitalocean_volume" "chromedp_vol" {
+    region 					= "sgp1"
+	name 					= "chromedp-vol"
+	size 					= 200
+	initial_filesystem_type = "ext4"
+	description				= "contains chromedp source tree"
+}
+
 resource "digitalocean_droplet" "chromedp_build" {
     image  = "debian-12-x64"
     name   = "chromedp-build"
     region = "sgp1"
-    size   = "c-16"
-    ssh_keys = [data.digitalocean_ssh_key.ssh_key.id]
+    size   = "s-1vcpu-2gb"
+    ssh_keys =  [data.digitalocean_ssh_key.ssh_key.id]
+}
+
+resource "digitalocean_volume_attachment" "chromedp_attach" {
+	droplet_id	= digitalocean_droplet.chromedp_build.id
+	volume_id	= digitalocean_volume.chromedp_vol.id
 }
 
 output "chromedp_build" {
-    value = digitalocean_droplet.chromedp_build.ipv4_address
+    value =  digitalocean_droplet.chromedp_build.ipv4_address
 }
